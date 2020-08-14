2db2ee2143fae088e58db052d03a54b6b147837e
Chirag Mhamunkar <chirag.mhamunkar@turtlemint.com>
2020-08-14 22:41:59 +0530

    Added controllers

---
+++ src/main/java/com/turtlemint/authservice/controller/PermissionController.java
@@ -0,0 +1,25 @@
+package com.turtlemint.authservice.controller;
+
+import com.turtlemint.authservice.entity.Permission;
+import com.turtlemint.authservice.repositories.PermissionRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+@Slf4j
+@RestController
+@RequestMapping("/permission")
+public class PermissionController {
+
+    @Autowired
+    private PermissionRepository permissionRepository;
+
+    @GetMapping
+    public Flux<Permission> fetchAllPermissions(){
+        log.info("Request received to fetch all permissions");
+        return permissionRepository.findAll();
+    }
+}
---
+++ src/main/java/com/turtlemint/authservice/controller/RoleController.java
@@ -0,0 +1,27 @@
+package com.turtlemint.authservice.controller;
+
+
+import com.turtlemint.authservice.entity.Role;
+import com.turtlemint.authservice.repositories.RoleRepository;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+@Slf4j
+@RestController
+@RequestMapping("/role")
+public class RoleController {
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @GetMapping
+    public Flux<Role> fetchAllRoles(){
+        log.info("Request received to fetch all roles.");
+        return roleRepository.findAll();
+    }
+}
